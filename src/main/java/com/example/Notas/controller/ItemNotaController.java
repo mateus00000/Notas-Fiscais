25c50ab2b52d20b4033a7d42ff56a7280f11b311
Mateus C <mateus.c000@outlook.com>
2024-08-07 20:15:38 -0300

    Finalização do crud de ItemNota

--- src/main/java/com/example/Notas/controller/ItemNotaController.java
+++ src/main/java/com/example/Notas/controller/ItemNotaController.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +52,35 @@ public class ItemNotaController {
             itensNotaDTOs.add(ItemNotaMapper.toDTO(itemNota));
         }
         return itensNotaDTOs;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemNotaDTO> obterItemNotaPorId(@PathVariable Long id) {
+        return itemNotaService.obterItemNotaPorId(id)
+            .map(itemNota -> ResponseEntity.ok(ItemNotaMapper.toDTO(itemNota)))
+            .orElse(ResponseEntity.notFound().build()); // Retorna erro 404 se não encontrado
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ItemNotaDTO> atualizarItemNota(@PathVariable Long id, @RequestBody ItemNotaDTO itemNotaDTO) {
+        Nota nota = notaService.obterNotaPorId(itemNotaDTO.getNotaId()).orElseThrow(() -> new RuntimeException("Nota não encontrada"));
+        Produto produto = produtoService.obterProdutosPorId(itemNotaDTO.getProdutoId()).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+        ItemNota itemNota = ItemNotaMapper.toEntity(itemNotaDTO, nota, produto);
+        ItemNota itemNotaAtualizado = itemNotaService.atualizarItemNota(id, itemNota);
+        if (itemNotaAtualizado != null) {
+            return ResponseEntity.ok(ItemNotaMapper.toDTO(itemNotaAtualizado));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarItemNota(@PathVariable Long id) {
+        boolean deletado = itemNotaService.deletarItemNota(id);
+        if (deletado) {
+            return ResponseEntity.noContent().build(); // Retorna 204 se a exclusão for bem-sucedida
+        } else {
+            return ResponseEntity.notFound().build(); // Retorna 404 se o itemNota não for encontrado
+        }
     }
 }
