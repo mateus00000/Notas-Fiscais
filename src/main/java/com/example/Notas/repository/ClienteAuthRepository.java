9e21eca208febae7266a42eb6a37f3ddca232bf4
Mateus C <mateus.c000@outlook.com>
2024-08-14 21:55:26 -0300

    aula cod segura

--- src/main/java/com/example/Notas/repository/ClienteAuthRepository.java
+++ src/main/java/com/example/Notas/repository/ClienteAuthRepository.java
@@ -2,6 +2,6 @@ package com.example.Notas.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.Notas.entities.ClienteAuth;
 
-public class ClienteAuthRepository extends JpaRepository <ClienteAuth, Long>{
+public interface ClienteAuthRepository extends JpaRepository <ClienteAuth, Long>{
     ClienteAuth findByUsername(String username);
 }
